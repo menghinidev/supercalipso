3f820c0b6b3768d549ca86e05fbf05d2277d8f41
Lorenzo Menghini <lomenghini@deloitte.it>
2022-10-14 11:55:53 +0200

    - Added new color palette

--- lib/presenter/theme/colors.dart
+++ lib/presenter/theme/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color blue = Color(0xFF0D8390);
+  /* static const Color blue = Color(0xFF0D8390);
   static const Color lightBlueBackground = Color(0xFFF2F4F8);
   static const Color green = Colors.lightGreen;
   static Color? get confirmColor => Colors.lightGreen[300];
@@ -9,5 +9,21 @@ class AppColors {
   static const Color coolGrey11 = Color(0xFF53565A);
   static const Color coolGrey4 = Color(0xFFBBBCBC);
   static const Color coolGrey10 = Color(0xFF63666A);
-  static const Color coolGrey7 = Color(0xFF97999B);
+  static const Color coolGrey7 = Color(0xFF97999B); */
+
+  static const Color pink = Color(0xFFfa9095);
+  static const Color pinkDarker = Color(0xFF885457);
+  static const Color orange = Color(0xFFfeb922);
+  static const Color orangeDarker = Color(0xFF8b681e);
+  static const Color blue = Color(0xFF8b681e);
+  static const Color blueDarker = Color(0xFF166c82);
+  static const Color green = Color(0xFF32c8bf);
+  static const Color greenDarker = Color(0xFF246f6c);
+
+  static const black = Color(0xFF373737);
+  static const greyDarker = Color(0xFF606769);
+  static const grey = Color(0xFF9aa0a3);
+  static const greyLighter = Color(0xFFbfc5c7);
+  static const whiteDarker = Color(0xFFe5e5e5);
+  static const white = Color(0xFFf5f7f9);
 }
